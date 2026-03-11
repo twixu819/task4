e1565adf825c0d9a093cef330bb3d529ad24cda2
dranefor <savelamaksim011@gmail.com>
2026-03-11 13:55:27 +0200

    Extend Main class and implement main method

--- src/main/java/ex03/Main.java
+++ src/main/java/ex03/Main.java
@@ -1,4 +1,14 @@
 package ex03;
 
-public class Main {
+import ex02.View;
+
+public class Main extends ex02.Main {
+    public Main(View view) {
+        super(view);
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main(new ViewableTable().getView());
+        main.menu();
+    }
 }
