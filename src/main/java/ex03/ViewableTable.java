570910f700eda5f10eb1ca9428c6a82d2cfc52ac
dranefor <savelamaksim011@gmail.com>
2026-03-11 13:57:47 +0200

    Extend ViewableTable from ViewableResult

--- src/main/java/ex03/ViewableTable.java
+++ src/main/java/ex03/ViewableTable.java
@@ -1,4 +1,9 @@
 package ex03;
+import ex02.View;
+import ex02.ViewableResult;
 
-public class ViewableTable {
+public class ViewableTable extends ViewableResult {
+    public View getView() {
+        return new ViewTable();
+    }
 }
