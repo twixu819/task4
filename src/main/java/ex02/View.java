86ebcce0470bcecffc9d40cb54f728734b178bd6
dranefor <savelamaksim011@gmail.com>
2026-03-11 13:52:52 +0200

    Refactor View class to interface with methods

--- src/main/java/ex02/View.java
+++ src/main/java/ex02/View.java
@@ -1,4 +1,12 @@
 package ex02;
+import java.io.IOException;
 
-public class View {
+public interface View {
+    public void viewHeader();
+    public void viewBody();
+    public void viewFooter();
+    public void viewShow();
+    public void viewInit();
+    public void viewSave() throws IOException;
+    public void viewRestore() throws Exception;
 }
